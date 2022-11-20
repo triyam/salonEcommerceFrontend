004f5d35c07bcfe831b21a5e919403b2ec611357
Srijan Verma <srijanv9@gmail.com>
2022-11-20 15:30:28 +0530

    mobiscroll

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Eventcalendar, getJson, toast } from "@mobiscroll/react";
+import { React } from "react";
 
 function App() {
+  const [myEvents, setEvents] = React.useState([]);
+
+  React.useEffect(() => {
+    getJson(
+      "https://trial.mobiscroll.com/events/?vers=5",
+      (events) => {
+        setEvents(events);
+      },
+      "jsonp"
+    );
+  }, []);
+
+  const onEventClick = React.useCallback((event) => {
+    toast({
+      message: event.event.title,
+    });
+  }, []);
+
+  const view = React.useMemo(() => {
+    return {
+      calendar: { labels: true },
+    };
+  }, []);
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Eventcalendar
+      theme="ios"
+      themeVariant="light"
+      clickToCreate={false}
+      dragToCreate={false}
+      dragToMove={false}
+      dragToResize={false}
+      eventDelete={false}
+      data={myEvents}
+      view={view}
+      onEventClick={onEventClick}
+    />
   );
 }
 
